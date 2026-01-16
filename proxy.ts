4cb2cf493722dcde94638ec61478b6cb238df489
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-16 22:04:33 +0900

    feat: redirect unauth access to auth path, and auth access to login path

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,38 @@
 // https://next-intl.dev/docs/routing/setup#proxy
 
+import {NextResponse} from "next/dist/server/web/spec-extension/response";
+import {NextRequest} from "next/dist/server/web/spec-extension/request";
 import createMiddleware from "next-intl/middleware";
-import {routing} from "./il8n/routing";
+import {routing} from "@/il8n/routing";
 
-export default createMiddleware(routing);
+const nextIntlMiddleware = createMiddleware(routing);
+
+export default function proxy(request: NextRequest) {
+    const sessionCookie = request.cookies.get('SESSION')
+    const {pathname} = request.nextUrl
+
+    type Locale = (typeof routing.locales)[number];
+
+    const isLocale = (value: string): value is Locale =>
+        routing.locales.includes(value as Locale);
+
+    const segment = pathname.split('/')[1];
+    const locale = isLocale(segment) ? segment : routing.defaultLocale;
+    const pathWithoutLocale = pathname.replace(new RegExp(`^/${locale}`), '') || '/';
+
+
+    // 1. If user IS authenticated and trying to access (unauth) routes
+    if (sessionCookie && (pathWithoutLocale.startsWith('/login'))) {
+        return NextResponse.redirect(new URL(`/${locale}/report`, request.url))
+    }
+
+    // 2. If user is NOT authenticated and trying to access (auth) routes
+    if (!sessionCookie && pathWithoutLocale.startsWith('/report')) {
+        return NextResponse.redirect(new URL(`/${locale}/login`, request.url))
+    }
+
+    return nextIntlMiddleware(request)
+}
 
 export const config = {
     // Match all pathnames except for

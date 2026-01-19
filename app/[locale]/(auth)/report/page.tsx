d47d5c5334b2ccedc4b3b4e85f2e0f8a6da8aaeb
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-19 17:18:44 +0900

    feat: enable subscription report fetch instead of sample report

--- app/[locale]/(auth)/report/page.tsx
+++ app/[locale]/(auth)/report/page.tsx
@@ -1,41 +1,39 @@
-import {getTranslations} from "next-intl/server";
-import {Suspense} from "react";
-import {Section} from "@/components/ui/section";
-import {Container} from "@/components/ui/container";
-import {subMonths} from "date-fns";
-import {ReportUpdateEligibilitySchema} from "@/lib/dto/dto";
-import {getUpdateEligibility} from "@/lib/api";
-import {redirect} from 'next/navigation'
-import {DefaultLayout} from "@/templates/DefaultLayout";
 import SubscriptionReport from "@/app/[locale]/(auth)/report/SubscriptionReport";
 import ErrorPage from "@/components/shared/ErrorPage";
+import { Container } from "@/components/ui/container";
+import { Section } from "@/components/ui/section";
+import { getReport, getUpdateEligibility } from "@/lib/api";
+import { ReportUpdateEligibilitySchema, SubscriptionReportSchema } from "@/lib/dto/dto";
+import { DefaultLayout } from "@/templates/DefaultLayout";
+import { subMonths } from "date-fns";
+import { getTranslations } from "next-intl/server";
+import { redirect } from 'next/navigation';
+import { Suspense } from "react";
 
 
 type Props = {
     params: Promise<{ locale: string }>;
 };
 
-const AnalysisReportPage = async ({params}: Props) => {
-
-    // const appUser : Promise<SubscriptionReport> = await getAppUser()
+const AnalysisReportPage = async ({ params }: Props) => {
 
     const t = await getTranslations("report");
 
-    // const response = await getReport()
-    //
-    // if (response.status === 401) {
-    //     redirect(`/login`)
-    // }
-    // if (response.status === 204) {
-    //     redirect(`/report/new`)
-    // }
-    // if (response.error) {
-    //     return <ErrorPage status={response.status} pageTitle={t("title")}/>
-    // }
-    //
-    // const report = SubscriptionReportSchema.parse(response.data)
+    const response = await getReport()
 
-    const report = sampleReport
+    if (response.status === 401) {
+        redirect(`/login`)
+    }
+    if (response.status === 204) {
+        redirect(`/report/new`)
+    }
+    if (response.error) {
+        return <ErrorPage status={response.status} pageTitle={t("title")} />
+    }
+
+    const report = SubscriptionReportSchema.parse(response.data)
+
+    //     const report = sampleReport
 
     const updateEligibilityResponse = await getUpdateEligibility()
 
@@ -43,7 +41,7 @@ const AnalysisReportPage = async ({params}: Props) => {
         redirect(`/login`)
     }
     if (updateEligibilityResponse.error) {
-        return <ErrorPage status={updateEligibilityResponse.status} pageTitle={t("title")}/>
+        return <ErrorPage status={updateEligibilityResponse.status} pageTitle={t("title")} />
     }
 
     const reportUpdateEligibility = ReportUpdateEligibilitySchema.parse(updateEligibilityResponse.data)
@@ -59,7 +57,7 @@ const AnalysisReportPage = async ({params}: Props) => {
                     </div>
                     <Suspense>
                         <SubscriptionReport subscriptionReport={report}
-                                            reportUpdateEligibility={reportUpdateEligibility}/>
+                            reportUpdateEligibility={reportUpdateEligibility} />
                     </Suspense>
                 </Container>
             </Section>
@@ -197,116 +195,116 @@ const sampleReport = {
                 email: "hyeon.expression@gmail.com"
             },
         },
-            {
-                subscriptions: [{
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "Disney+",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "disneyplus.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 12),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: subMonths(Date.now(), 8),
-                    isNotSureIfSubscriptionIsOngoing: false
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "왓챠",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "watcha.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 12),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: subMonths(Date.now(), 8),
-                    isNotSureIfSubscriptionIsOngoing: true
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "티빙",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: false,
-                        logoDevSuffix: "tving.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 12),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: null,
-                    isNotSureIfSubscriptionIsOngoing: false
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "ChatGPT Pro",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "chatgpt.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 12),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: subMonths(Date.now(), 8),
-                    isNotSureIfSubscriptionIsOngoing: true
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "Figma",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "figma.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 12),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: null,
-                    isNotSureIfSubscriptionIsOngoing: false
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "쿠팡",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "coupang.com"
-                    },
-                    registeredSince: subMonths(Date.now(), 8),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: null,
-                    isNotSureIfSubscriptionIsOngoing: false
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "매우 긴 서비스 이름 가나다라마바사아자차카타파하",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "navercorp.vn"
-                    },
-                    registeredSince: subMonths(Date.now(), 8),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: null,
-                    isNotSureIfSubscriptionIsOngoing: false
-                }, {
-                    serviceProvider: {
-                        id: "fakeId",
-                        displayName: "매우 긴 서비스 이름 가나다라마바사아자차카타파하",
-                        websiteUrl: "https://nid.naver.com/membership/join",
-                        subscriptionPageUrl: "https://nid.naver.com/membership/join",
-                        canAnalyzeSubscription: true,
-                        logoDevSuffix: "navercorp.vn"
-                    },
-                    registeredSince: subMonths(Date.now(), 8),
-                    hasSubscribedNewsletterOrAd: false,
-                    subscribedSince: subMonths(Date.now(), 7),
-                    isNotSureIfSubscriptionIsOngoing: false
-                }],
-                googleAccount: {
-                    name: "HYEON",
-                    email: "very long email address abcdefghijkelmopqrstuvwxyz@gmail.com"
+        {
+            subscriptions: [{
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "Disney+",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "disneyplus.com"
                 },
-            }
+                registeredSince: subMonths(Date.now(), 12),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: subMonths(Date.now(), 8),
+                isNotSureIfSubscriptionIsOngoing: false
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "왓챠",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "watcha.com"
+                },
+                registeredSince: subMonths(Date.now(), 12),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: subMonths(Date.now(), 8),
+                isNotSureIfSubscriptionIsOngoing: true
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "티빙",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: false,
+                    logoDevSuffix: "tving.com"
+                },
+                registeredSince: subMonths(Date.now(), 12),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: null,
+                isNotSureIfSubscriptionIsOngoing: false
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "ChatGPT Pro",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "chatgpt.com"
+                },
+                registeredSince: subMonths(Date.now(), 12),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: subMonths(Date.now(), 8),
+                isNotSureIfSubscriptionIsOngoing: true
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "Figma",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "figma.com"
+                },
+                registeredSince: subMonths(Date.now(), 12),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: null,
+                isNotSureIfSubscriptionIsOngoing: false
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "쿠팡",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "coupang.com"
+                },
+                registeredSince: subMonths(Date.now(), 8),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: null,
+                isNotSureIfSubscriptionIsOngoing: false
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "매우 긴 서비스 이름 가나다라마바사아자차카타파하",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "navercorp.vn"
+                },
+                registeredSince: subMonths(Date.now(), 8),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: null,
+                isNotSureIfSubscriptionIsOngoing: false
+            }, {
+                serviceProvider: {
+                    id: "fakeId",
+                    displayName: "매우 긴 서비스 이름 가나다라마바사아자차카타파하",
+                    websiteUrl: "https://nid.naver.com/membership/join",
+                    subscriptionPageUrl: "https://nid.naver.com/membership/join",
+                    canAnalyzeSubscription: true,
+                    logoDevSuffix: "navercorp.vn"
+                },
+                registeredSince: subMonths(Date.now(), 8),
+                hasSubscribedNewsletterOrAd: false,
+                subscribedSince: subMonths(Date.now(), 7),
+                isNotSureIfSubscriptionIsOngoing: false
+            }],
+            googleAccount: {
+                name: "HYEON",
+                email: "very long email address abcdefghijkelmopqrstuvwxyz@gmail.com"
+            },
+        }
         ]
 }
